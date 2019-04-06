d0ee78695230177ce094d7ba728a9c1bd7e062d0
Zack <zkeliani@vt.edu>
2019-04-05 23:30:41 -0400

    Free

--- hw3.c
+++ hw3.c
@@ -10,11 +10,13 @@ producer-consumer problem
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 
 #define MAX_JOBS 25
 #define MAX_USER_JOBS 30
 #define MIN_BYTES 100
 #define MAX_BYTES 1000
+#define SHMSZ 512
 
 typedef struct
 {
@@ -79,63 +81,146 @@ int min(int x, int y)
 
 typedef struct
 {
-	int val;
-	sem_t gate;
-	sem_t mutex;
-} CSem;
-
-CSem create_CSem(int x)
-{
-	CSem csem;
-	csem.val = x;
-	sem_init(&csem.gate, 1, min(1, csem.val));
-	sem_init(&csem.mutex, 1, 1);
-}
-
-void Pc(CSem cs)
-{
-	
-}
+	sem_t full, empty, rw;
+	int f_val, e_val, rw_val;
+} Semaphores;
 
 GlobalQueue global_queue;
-sem_t full, empty, rw;
+Semaphores semaphores;
+sem_t wait_sem, mutex_sem;
+int wait_val, mutex_val;
+
+void wait()
+{
+	sem_getvalue(&wait_sem,  &mutex_val);
+	sem_getvalue(&mutex_sem, &mutex_val);
+	
+	sem_wait(&wait_sem);
+	sem_wait(&mutex_sem);
+	global_queue.count = global_queue.count - 1;
+	if (global_queue.count > 0)
+	{
+		sem_post(&wait_sem);
+	}
+	sem_post(&mutex_sem);
+}
+
+void signal1()
+{
+	sem_getvalue(&wait_sem,  &mutex_val);
+	sem_getvalue(&mutex_sem, &mutex_val);
+	
+	sem_wait(&mutex_sem);
+	global_queue.count = global_queue.count + 1;
+	if (global_queue.count == 1)
+	{
+		sem_post(&wait_sem);
+	}
+	sem_post(&mutex_sem);
+}
+
+void * Count(void * r)
+{
+	int temp, nm;
+	for (nm = 0; nm < MAX_JOBS; nm++)
+	{
+		wait();
+		
+		signal1();
+	}
+}
+
+void handle_sigint(int sig)
+{
+	printf("Caught signal %d, threads terminated\n", sig);
+}
 
 int main(int argc, char *argv[])
 {
-	int x, y;
-	int arg1, arg2;
+	sem_init(&semaphores.full, 1, 0);
+	sem_init(&semaphores.empty, 1, 0);
+	sem_init(&semaphores.rw, 1, 0);
+	sem_init(&wait_sem, 1, min(1, global_queue.count));
+	sem_init(&mutex_sem, 1, 1);
+	
+	pid_t f1 = fork();
+	
+	if (f1 != 0)
+	{
+		int x, y;
+		int arg1, arg2;
+		
+		printf("Please enter the number of producer(user) processes: \n");
+		if ((arg1 = scanf("%d", &x)) == 0)
+		{
+			printf("Error: Input must be an integer.\n");
+			exit(1);
+		}
+		
+		printf("Please enter the number of printer(consumer) threads: \n");
+		if ((arg2 = scanf("%d", &y)) == 0)
+		{
+			printf("Error: Input must be an integer.\n");
+			exit(1);
+		}
+		
+		//for seeding random functions
+		srand(time(NULL));
+		
+		User users[x];
+		
+		int q;
+		for (q = 0; q < x; q++)
+		{
+			User temp = create_user();
+			users[q] = temp;
+		}
+		
+		global_queue.count = 0;
+		
+		sem_init(&semaphores.full, 1, 0);
+		sem_init(&semaphores.empty, 1, 0);
+		sem_init(&semaphores.rw, 1, 0);
+		sem_init(&wait_sem, 1, min(1, global_queue.count));
+		sem_init(&mutex_sem, 1, 1);
+		
+		pthread_t threads[y];
 
-	printf("Please enter the number of producer(user) processes: \n");
-	if ((arg1 = scanf("%d", &x)) == 0)
+		int o;
+		for(o = 0; o < q; o++)
+		{
+			fork();
+		}
+		
+		signal(2, handle_sigint);
+		
+		int totalJobs;
+		totalJobs = 0;
+		int iter;
+		int iter2;
+		for (iter = 0; iter < x; iter++)
+		{
+			totalJobs = users[iter].remaining_jobs;
+		}
+		
+		printf("Total print jobs: %d\n", totalJobs);
+		
+		for (iter = 0; iter < x; iter++)
+		{
+			for (iter2 = 0; iter2 < users[iter].remaining_jobs; iter2++)
+			{
+				printf("Job size: %d\n", users[iter].jobs[iter2].bytes);
+			}
+		}
+		
+		
+	}
+	else
 	{
-		printf("Error: Input must be an integer.\n");
-		exit(1);
+
 	}
 	
-	printf("Please enter the number of printer(consumer) threads: \n");
-	if ((arg2 = scanf("%d", &y)) == 0)
-	{
-		printf("Error: Input must be an integer.\n");
-		exit(1);
-	}
 	
-	//for seeding random functions
-	srand(time(NULL));
 	
-	User users[x];
-	
-	int q;
-	for (q = 0; q < x; q++)
-	{
-		User temp = create_user();
-		users[q] = temp;
-	}
-	
-	global_queue.count = 0;
-	
-	sem_init(&full, 1, 0);
-	sem_init(&empty, 1, 0);
-	sem_init(&rw, 1, 0);
-	
-	pthread_t tid1, tid2, tid3;
+	return 0;
 }
